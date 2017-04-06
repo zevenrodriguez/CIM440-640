1f5bfb65f2bb20db44ff56f4c4587cf10af46aa7
Zevensuy Rodriguez <zevenwolf@gmail.com>
2017-04-06 10:57:53 -0400

    week12

--- week11/classExamples/bubbleMulti/sketch.js
+++ week11/classExamples/bubbleMulti/sketch.js
@@ -5,10 +5,10 @@ var bubbles = [];
 function setup() {
     createCanvas(400, 400);
     bubble0 = new bubble(100, 100, 100);
-    bubble1 = new bubble(200,200,100);
+    bubble1 = new bubble(200, 200, 100);
 
-    for(var i = 0; i < 100; i++){
-        bubbles.push(new bubble(random(width), random(height), random(10,100)));
+    for (var i = 0; i < 100; i++) {
+        bubbles.push(new bubble(random(width), random(height), random(10, 100), random(1,10)));
     }
 
     console.log(bubbles.length);
@@ -17,38 +17,41 @@ function setup() {
 function draw() {
     background(255);
 
-//    bubble0.display();
-//    bubble0.move();
-//    var checkBubble = bubble0.checkPos();
-//
-//    if(checkBubble == true){
-//        bubble0.x = 0;
-//    }
-//
-//    bubbles[0].display();
-//    bubbles[0].move();
-
-
-    for(var i = 0; i < bubbles.length; i++){
+    for (var i = 0; i < bubbles.length; i++) {
         bubbles[i].display();
+        bubbles[i].move();
+        var checkBubble = bubbles[i].checkPos();
+
+        if (checkBubble == true) {
+            bubbles[i].x = 0;
+        }
     }
 
 
 }
 
 
-function bubble(tempX, tempY, tempDiameter) {
+function bubble(tempX, tempY, tempDiameter, tempSpeed) {
 
     this.x = tempX;
     this.y = tempY;
     this.diameter = tempDiameter;
+    this.speed = tempSpeed;
+
+    this.r = random(255);
+    this.g = random(255);
+    this.b = random(255);
+    this.alpha = random(255);
+
+
 
     this.display = function () {
+        fill(this.r,this.g,this.b, this.alpha);
         ellipse(this.x, this.y, this.diameter, this.diameter);
     }
 
     this.move = function () {
-        this.x++;
+        this.x = this.x + this.speed;
     }
 
     this.checkPos = function () {
---
+++ week12/readme.md
@@ -0,0 +1,15 @@
+
+### Adding a library:
+
+* Add library to your assets folder.
+  * Drag and drop your file into the assets folder.
+  * Make sure that you file end in a .js extension.
+* Click on View => Toggle Sidebar
+  * Click on index.html
+  * Add  <script src=”libraries/yourLibraryHere.js” type=”text/javascript”></script>
+  * Include your library underneath the p5.js script tag
+
+
+## Homeworks
+
+Create a tutorial on a library that you would want to explore for your final project.  Create a sketch example using the library. Make it creative. Explain functions and logic used in your sketch. Be prepared to talk about the library.
