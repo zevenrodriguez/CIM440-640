fc38d2fc788c6018a05ccad5d899a7d7e3d8d20d
Zevensuy Rodriguez <zevenrodriguez@gmail.com>
2017-10-03 16:46:39 -0400

    cim640 notes

---
+++ week7/examples/cim640/gettingStarted/empty-example/sketch.js
@@ -0,0 +1,17 @@
+
+
+function setup(){
+    createCanvas(500,500);
+    background(125);
+    background(255,0,0);
+    background("purple");
+    background("#999999");
+
+
+}
+
+function draw(){
+
+    ellipse(250,250,100,100);
+
+}
