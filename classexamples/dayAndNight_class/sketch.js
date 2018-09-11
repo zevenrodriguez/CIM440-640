dbf143a6523b3eeda83891ad18c77b79634a8a7e
Zevensuy Rodriguez <zevenrodriguez@gmail.com>
2018-09-11 18:29:28 -0400

    week4

--- classexamples/dayAndNight_class/sketch.js
+++ classexamples/dayAndNight_class/sketch.js
@@ -1,7 +1,9 @@
 var sun;
+var moon;
 
 function preload(){
   sun = loadImage('assets/sun.png');
+  moon = loadImage('assets/moon.png');
 }
 
 function setup() {
@@ -11,5 +13,40 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  image(sun,100,100);
+
+  background(255);
+  console.log("mouseX: " + mouseX + " mouseY: " + mouseY);
+  //Interface
+  textSize(18);
+  text("Move over each area to reveal a surprise!", 20,20);
+  line(width/2, 0, width/2, height);
+  text("Day", 120,360);
+  text("Night", 300,360);
+
+  //Interaction
+
+
+  /*
+  if(true){
+    Execute this code
+  }
+  */
+
+  if(mouseX < width/2){
+    console.log("Day Side");
+    image(sun,100,100);
+  }
+
+  if(mouseX >= width/2){
+    console.log("Night Side");
+    image(moon,100,100);
+  }
+
+
+
+
+
+
+
+  //end of code
 }
