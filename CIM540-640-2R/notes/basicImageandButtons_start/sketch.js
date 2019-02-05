370729312bc4566cfa0c147b02fb56d7a19bdeb3
Zevensuy Rodriguez <zevenrodriguez@gmail.com>
2019-02-05 16:42:48 -0500

    week4

--- CIM540-640-2R/notes/basicImageandButtons_start/sketch.js
+++ CIM540-640-2R/notes/basicImageandButtons_start/sketch.js
@@ -1,16 +1,54 @@
 /* "Up-Close Sloth" by marissa_strniste, "Baby sloth, being cute" by Dave Gingrich is licensed under CC BY-SA 2.0. To view a copy of this license, visit: https://creativecommons.org/licenses/by-sa/2.0*/
 
+var slothBaby;
+var slothAdult;
+
+var babyButton;
+var adultButton;
+
+var currentImage = 1;
 
 function preload(){
-
+  slothBaby = loadImage("assets/slothbaby.jpg");
+  slothAdult = loadImage("assets/slothadult.jpg");
 }
 
 function setup() {
   // put setup code here
+  createCanvas(500,500);
+  babyButton = createButton("Baby sloth");
+  babyButton.position(10,40);
+  babyButton.mousePressed(function(){
+    currentImage = 0;
+  });
+
+  adultButton = createButton("Adult Sloth");
+  adultButton.position(100,40);
+  adultButton.mousePressed(adultChange);
 
 }
 
 function draw() {
   // put drawing code here
+  if(currentImage == 0){
+    image(slothBaby, 0,20,slothBaby.width/2,slothBaby.height/2);
+  }else if(currentImage == 1){
+    image(slothAdult, 0,20,slothAdult.width/2,slothAdult.height/2);
+  }
+
+
 
 }
+
+function adultChange(){
+  currentImage = 1;
+}
+
+
+
+
+
+
+
+
+// end of code
--- CIM540-640-2R/notes/coloringBookHitzones_start/sketch.js
+++ CIM540-640-2R/notes/coloringBookHitzones_start/sketch.js
@@ -1,14 +1,62 @@
+var brushColor = 0;
+var brushSize = 10;
+
+var circleButtonX = 100;
+var circleButtonY = 450;
+var circleButtonSize = 100;
+
+var rectButtonX = 200;
+var rectButtonY = 400;
+var rectButtonSize = 100;
+
+var robot;
 
 function preload(){
 
-
+robot = loadImage("assets/robot.png")
 }
 function setup() {
   // put setup code here
+  createCanvas(500,500);
 
 }
 
 function draw() {
   // put drawing code here
+  fill(brushColor);
+  ellipse(mouseX,mouseY,brushSize,brushSize);
 
+  //interface
+  fill(127);
+  ellipse(circleButtonX,circleButtonY,circleButtonSize,circleButtonSize);
+  fill(255);
+  text("clear", circleButtonX,circleButtonY);
+  var circleDist = dist(circleButtonX,circleButtonY,mouseX,mouseY);
+  console.log("circleDist: " + circleDist);
+
+  if(circleDist < circleButtonSize/2 && mouseIsPressed == true){
+    background(255);
+  }
+
+  fill(255,0,0);
+  rect(rectButtonX,rectButtonY,rectButtonSize,rectButtonSize);
+  fill(255);
+  text("red", rectButtonX + 50,rectButtonY + 50);
+
+  if(mouseX > rectButtonX && mouseX < rectButtonX + rectButtonSize && mouseY > rectButtonY && mouseY < rectButtonY + rectButtonSize && mouseIsPressed == true){
+      brushColor = color(255,0,0);
+  }
+
+  image(robot, 0 ,0);
 }
+
+
+
+
+
+
+
+
+
+
+//end of code
