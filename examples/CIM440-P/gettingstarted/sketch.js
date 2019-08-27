44de8b25fa58a2afd9d64c1e0c4420680105e6d6
Zevensuy Rodriguez <zevenrodriguez@gmail.com>
2019-08-27 12:14:06 -0400

    getting started

---
+++ examples/CIM440-P/gettingstarted/sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  // put setup code here
+
+  createCanvas(500,500);
+  background("green");
+  background("LemonChiffon");
+  background(127);//Medium gray color, gray scale goes from 0-255
+  background(0);//black
+  background(255);//white
+  background(0, 127);// black with a 127 or 50% opacity
+  background("#FFFACD");
+  background(255,0,0);//red
+  background(0,255,0);//green
+  background(0,0,255);//blue
+  background(0,128,128);//teal
+
+  fill("cyan");
+  stroke(255,0,0);
+  ellipse(50,50,50,50);
+
+  fill(0,255,0);
+  ellipse(100,100,25,25);
+
+
+}
+
+function draw() {
+  // put drawing code here
+}
