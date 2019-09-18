c290dd81e2ee52a2becf9b787a97c8ab069b74a9
Zevensuy Rodriguez <zevenrodriguez@gmail.com>
2019-09-18 12:20:22 -0400

    ifGallery

---
+++ examples/ifGallery/sketch.js
@@ -0,0 +1,11 @@
+// Sloth image "IMG_3883" by JohnHedtke is licensed under CC BY-NC-SA 2.0 https://search.creativecommons.org/photos/217faedd-d06a-48ba-91f7-f4d4c6dafa04
+// Puppy image "puppies" by www.petian.net is licensed under CC BY-NC-ND 2.0  https://search.creativecommons.org/photos/4f271583-e45d-4faf-bfc8-7291c7c1e3c3
+// dinosaur "Rupenhorn Dinosaur #4" by Kristian_Laban is licensed under CC BY-NC 2.0 https://search.creativecommons.org/photos/96213ba5-e9b6-4f2f-91e4-da28b9fcad22
+
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
